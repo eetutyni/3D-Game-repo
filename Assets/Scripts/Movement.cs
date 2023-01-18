fd67c0979ba2c70bf927b11296929acb72d15ed7
Rappe34 <92690091+Rappe34@users.noreply.github.com>
2023-01-18 10:39:41 +0200

    Revert "Map and script fixes"
    This reverts commit 0dbfb0d07507dc862cc04546712b37d2d8e4daa3.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,30 +7,17 @@ public class Movement : MonoBehaviour
 
     public CharacterController controller;
 
-<<<<<<< Updated upstream
     public float speed = 12f;
     public float gravity = -9.81f;
     Vector3 velocity;
-=======
-    private Vector3 velocity;
-    private Vector3 move;
-
-    private float x, z;
-    
->>>>>>> Stashed changes
     bool isGrounded;
     public float jumpHeight = 3f;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
-<<<<<<< Updated upstream
   
     void Update()
-=======
-
-    void FixedUpdate()
->>>>>>> Stashed changes
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
@@ -39,23 +26,19 @@ public class Movement : MonoBehaviour
             velocity.y = -2f;
         }
 
-        x = Input.GetAxis("Horizontal");
-        z = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
 
-        move = transform.right * x + transform.forward * z;
+        Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
 
         velocity.y += gravity * Time.deltaTime;
 
-<<<<<<< Updated upstream
         if(Input.GetButtonDown("Jump")&& isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
-=======
-        if(Input.GetButtonDown("Jump") && isGrounded) Jump();
->>>>>>> Stashed changes
 
         controller.Move(velocity * Time.deltaTime);
     }
