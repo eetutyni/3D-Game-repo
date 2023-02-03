ab14ba842af7d7c1bb887582ee9eae13a27f1920
eetutyni <118897623+eetutyni@users.noreply.github.com>
2023-02-03 10:00:23 +0200

    Main Menu

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartButton()
+    {
+
+
+    }
+
+    public void SettingsButton()
+    {
+        SceneManager.LoadScene("Settings");
+
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+
+
+
+
+
+
+
+
+}
