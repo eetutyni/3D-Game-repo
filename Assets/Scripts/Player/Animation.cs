f1ac68ab2f0733792ed6ae6395a5923f44533eeb
Rappe34 <rappe34@outlook.com>
2023-02-06 09:31:21 +0200

    Made images for inventoryitems

--- Assets/Scripts/Player/Animation.cs
+++ Assets/Scripts/Player/Animation.cs
@@ -13,10 +13,10 @@ public class Animation : MonoBehaviour
     void Update()
     {
         if (movementScript.hasJumped) animator.Play("Jump");
-        else if (Input.GetKey(KeyCode.W) && movementScript.move.magnitude > 0.1f && movementScript.isGrounded) animator.Play("RunForward");
-        else if (Input.GetKey(KeyCode.S) && movementScript.move.magnitude > 0.1f && movementScript.isGrounded) animator.Play("RunBackward");
-        else if (Input.GetKey(KeyCode.A) && movementScript.move.magnitude > 0.1f && movementScript.isGrounded) animator.Play("StrafeLeft");
-        else if (Input.GetKey(KeyCode.D) && movementScript.move.magnitude > 0.1f && movementScript.isGrounded) animator.Play("StrafeRight");
+        else if (movementScript.move.x > 0.1 && movementScript.move.magnitude > 0.1f && movementScript.isGrounded) animator.Play("RunForward");
+        else if (movementScript.move.x < 0.1 && movementScript.move.magnitude > 0.1f && movementScript.isGrounded) animator.Play("RunBackward");
+        else if (movementScript.move.y < 0.1 && movementScript.move.magnitude > 0.1f && movementScript.isGrounded) animator.Play("StrafeLeft");
+        else if (movementScript.move.y > 0.1 && movementScript.move.magnitude > 0.1f && movementScript.isGrounded) animator.Play("StrafeRight");
         else if (movementScript.move.magnitude < 0.1f && movementScript.isGrounded) animator.Play("Idle");
     }
 }
