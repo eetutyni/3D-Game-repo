733500b0b7f0ddc253f382a65d9962f95a9f3d10
eetutyni <118897623+eetutyni@users.noreply.github.com>
2023-02-28 09:58:36 +0200

    imported houses and fences

---
+++ Assets/Scripts/Collectible/Parts.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using TMPro;
+using UnityEngine;
+
+public class Parts : MonoBehaviour
+{
+    int parts = 0;
+
+    int timer = 0;
+
+    [SerializeField] TextMeshProUGUI partsText;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("part"))
+        {
+            partsText.gameObject.SetActive(true);
+            Destroy(other.gameObject);
+            parts++;
+            partsText.text = "Parts: " + parts + " / 4";
+            StartCoroutine(Waiter());
+        }
+    }
+    
+    private IEnumerator Waiter()
+    {
+        yield return new WaitForSeconds(4);
+            partsText.gameObject.SetActive(false);
+    }
+}
--- Assets/Scripts/Inventory/CheckForItem.cs
+++ Assets/Scripts/Inventory/CheckForItem.cs
@@ -1,19 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class CheckForItem : MonoBehaviour
 {
     public bool canCollect = false;
     public GameObject hitobj;
 
+    [SerializeField] GameObject storyPanel;
+
     private void FixedUpdate()
     {
         Ray ray = new Ray(transform.position, transform.forward);
 
         if (Physics.Raycast(ray, out RaycastHit hit, 3f))
+        {
             hitobj = hit.collider.gameObject;
+        }
+            
         else hitobj = null;
+
+        if (hitobj.tag == "paper") storyPanel.SetActive(true);
+        else storyPanel.SetActive(false);
 
         if (hitobj != null && hitobj.layer == 8) canCollect = true;
         else canCollect = false;
