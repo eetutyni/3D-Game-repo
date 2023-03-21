451d6177c47b9ff28fa6e1df4137b3bab943789d
eetutyni <118897623+eetutyni@users.noreply.github.com>
2023-03-21 08:48:35 +0200

    Update Animation.cs

--- Assets/Scripts/Player/Animation.cs
+++ Assets/Scripts/Player/Animation.cs
@@ -12,30 +12,11 @@ public class Animation : MonoBehaviour
 
     void Update()
     {
-        if (movementScript.hasJumped) animator.SetTrigger("Jump");
-        if (movementScript.move.x > 0.1 && movementScript.move.magnitude > 0.1f && movementScript.isGrounded)
-        {
-            
-        }
-        if (movementScript.move.x < 0.1 && movementScript.move.magnitude > 0.1f && movementScript.isGrounded)
-        {
-           
-        }
-        if (movementScript.move.y < 0.1 && movementScript.move.magnitude > 0.1f && movementScript.isGrounded)
-        {
-            
-        }
-        if (movementScript.move.y > 0.1 && movementScript.move.magnitude > 0.1f && movementScript.isGrounded)
-        {
-            
-        }
-        if (movementScript.move.magnitude < 0.1f && movementScript.isGrounded)
-        {
-   
-        }
         if (Input.GetButtonDown("Fire1"))
         {
             animator.SetTrigger("attack");
         }
     }
-}
+        
+
+
