cb2d335387988b94b3ffe02a43c339c0f2953e74
eetutyni <118897623+eetutyni@users.noreply.github.com>
2023-03-24 08:49:35 +0200

    script changes

--- Assets/Scripts/Player/Animation.cs
+++ Assets/Scripts/Player/Animation.cs
@@ -18,59 +18,7 @@ public class Animation : MonoBehaviour
 
     void Update()
     {
-        // Set animation parameters based on movement direction
-        if (mov.move.magnitude > 0.1f)
-        {
-            animator.SetBool("idle", false);
-            if (Input.GetKeyDown(KeyCode.W) && mov.move.magnitude > 0.1f && mov.isGrounded)
-            {
-                animator.SetBool("forward", true);
-            }
-            else if (Input.GetKey(KeyCode.S) && mov.move.magnitude > 0.1f && mov.isGrounded)
-            {
-                animator.SetBool("backward", true);
-            }
-            else if (Input.GetKey(KeyCode.A) && mov.move.magnitude > 0.1f && mov.isGrounded)
-            {
-                animator.SetBool("left", true);
-            }
-            else if (Input.GetKey(KeyCode.D) && mov.move.magnitude > 0.1f && mov.isGrounded)
-            {
-                animator.SetBool("right", true);
-            }
-            /*if (Input.GetKeyDown(KeyCode.W) && Input.GetKey(KeyCode.A) && mov.move.magnitude > 0.1f && mov.isGrounded)
-            {
-                animator.SetBool("Sleft", true);
-            }
-            if (Input.GetKeyDown(KeyCode.W) && Input.GetKeyDown(KeyCode.D) && mov.move.magnitude > 0.1f && mov.isGrounded)
-            {
-                animator.SetBool("Sright", true);
-            }
-            */
-
-        }
-
-        
-        else
-        {
-            animator.SetBool("forward", false);
-            animator.SetBool("backward", false);
-            animator.SetBool("right", false);
-            animator.SetBool("left", false);
-            animator.SetBool("Sright", false);
-            animator.SetBool("Sleft", false);
-            animator.SetBool("idle", true);
-        }
-
-        // Set animation parameters for jump
-        if (mov.hasJumped)
-        {
-            animator.SetBool("jump", true);
-        }
-        else
-        {
-            animator.SetBool("jump", false);
-        }
+       
         if(Input.GetKeyDown(KeyCode.Mouse0)) 
         { 
             animator.SetBool("attack", true);
